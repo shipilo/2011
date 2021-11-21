c388326105119611652d86238ebe3d595d3ee7ee
shipilo <shadowfire-1@mail.ru>
2021-11-21 11:27:02 +0300

    Добавьте файлы проекта.

---
+++ Met_2011/Creation.cs
@@ -0,0 +1,9 @@
+﻿namespace Met_2011
+{
+    abstract class Creation
+    {
+        protected int index;
+
+        public int Index { get => index; }
+    }
+}
---
+++ Met_2011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Met_2011
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упражнение 11.");
+            BankFabric.DeleteAccount(BankFabric.CreateAccount());
+
+            Console.WriteLine("Домашнее задание 11.");
+            Creator.DeleteAccount(BankFabric.CreateAccount());
+
+            Console.ReadLine();
+        }
+    }
+}
